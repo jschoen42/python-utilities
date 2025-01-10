07e82a9e7cee3a3fbe547a54170286d11500e8d3
Jürgen Schoenemeyer <108626845+jschoen42@users.noreply.github.com>
2025-01-10 12:37:38 +0100

    add mypy settings

--- __mypy.py
+++ __mypy.py
@@ -13,17 +13,36 @@ BASE_PATH = Path(sys.argv[0]).parent.parent.resolve()
 
 def run_mypy() -> None:
 
+    # https://mypy.readthedocs.io/en/stable/command_line.html
+
     settings: List[str] = [
+        # Incremental mode
+        "--sqlite-cache",
+
+        # Untyped definitions and calls
         "--disallow-untyped-calls",
         "--disallow-untyped-defs",
-        "--disallow-incomplete-defs",
         "--disallow-untyped-decorators",
+        "--disallow-incomplete-defs",
 
+        # Configuring warnings
         "--warn-redundant-casts",
         "--warn-unused-ignores",
         "--warn-unreachable",
 
-        "--sqlite-cache",
+        # Miscellaneous strictness flags
+        "--strict-equality",
+
+        # Configuring error messages
+        # "--show-error-context"
+        "--show-column-numbers",
+        "--show-error-end",
+        # "--show-error-code-links".
+        # "--pretty",
+        "--force-uppercase-builtins",
+
+        # Advanced options
+        "--show-traceback",
     ]
 
     filepath = Path(sys.argv[1]).stem
